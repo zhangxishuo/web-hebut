eddf7bc6429bf0498bb8b0fb5b374397286a7e8e
zhangxishuo <zhangxishuo1998@Gmail.com>
2018-11-08 16:52:33 +0800

    format-code

--- html-css-apply/table/app/scripts/controllers/main.js
+++ html-css-apply/table/app/scripts/controllers/main.js
@@ -8,62 +8,62 @@
  * Controller of the webApp
  */
 angular.module('webApp')
-  .controller('MainCtrl', function($scope, $http) {
-    var self = this;
+    .controller('MainCtrl', function($scope, $http) {
+        var self = this;
 
-    self.init = function() {
-      self.getAllStudents();
-    };
+        self.init = function() {
+            self.getAllStudents();
+        };
 
-    self.getAllStudents = function() {
-      $http.get('data/students.json')
-        .then(function success(response) {
-          $scope.list = response.data;
-          self.render($scope.list);
-        }, function error() {
-          console.log('数据请求失败');
-        });
-    };
+        self.getAllStudents = function() {
+            $http.get('data/students.json')
+                .then(function success(response) {
+                    $scope.list = response.data;
+                    self.render($scope.list);
+                }, function error() {
+                    console.log('数据请求失败');
+                });
+        };
 
-    self.remove = function(stu) {
-      $scope.list = $scope.list.filter(function(value) {
-        return value.id !== stu.id;
-      });
-      self.render($scope.list);
-    };
+        self.remove = function(stu) {
+            $scope.list = $scope.list.filter(function(value) {
+                return value.id !== stu.id;
+            });
+            self.render($scope.list);
+        };
 
-    self.render = function(list) {
-      var names = [];
-      var scores = [];
-      angular.forEach(list, function(value) {
-        names.push(value.name);
-        scores.push(value.score);
-      });
+        self.render = function(list) {
+            var names = [];
+            var scores = [];
+            angular.forEach(list, function(value) {
+                names.push(value.name);
+                scores.push(value.score);
+            });
 
-      var chart = echarts.init(document.getElementById('chart'));
+            var chart = echarts.init(document.getElementById('chart'));
 
-      var option = {
-        title: {
-          text: '学生成绩分析图表'
-        },
-        legend: {
-          data: ['成绩']
-        },
-        xAxis: {
-          data: names
-        },
-        yAxis: {},
-        series: [{
-          name: '成绩',
-          type: 'bar',
-          data: scores
-        }]
-      };
+            var option = {
+                title: {
+                    text: '学生成绩分析图表'
+                },
+                legend: {
+                    data: ['成绩']
+                },
+                xAxis: {
+                    data: names
+                },
+                yAxis: {},
+                series: [{
+                    name: '成绩',
+                    type: 'bar',
+                    data: scores
+                }]
+            };
 
-      chart.setOption(option);
-    };
+            chart.setOption(option);
+        };
 
-    $scope.remove = self.remove;
+        $scope.remove = self.remove;
 
-    self.init();
-  });
+        self.init();
+    });
